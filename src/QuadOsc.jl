3039fcb3b1c422d213a577dcc3d1fa21cce0a37b
Henry Gebhardt <hsggebhardt@gmail.com>
2020-11-21 23:36:25 -0800

    fix indentation

--- src/QuadOsc.jl
+++ src/QuadOsc.jl
@@ -7,55 +7,60 @@ using .SeriesAccelerations
 using QuadGK
 
 
-# Integrate from 'a' to 'b'.
+@doc raw"""
+    quadosc()
+"""
 function quadosc(f::Function, a::Number, b::Number, zeros::Function; pren=2,
 		 atol=zero(Float64), rtol=sqrt(eps(Float64)), order=7,
 		 nconvergences=ceil(Int,-1.31*log10(rtol))-pren)
-	@assert b == Inf
-	T = Float64
+    @assert b == Inf
+    T = Float64
 
-	i1 = findfirst(n -> zeros(n) - a >= 0, 1:typemax(Int))
-	presum, Err = quadgk(f, a, zeros(i1); atol=atol, rtol=rtol, order=order)
+    i1 = findfirst(n -> zeros(n) - a >= 0, 1:typemax(Int))
+    presum, Err = quadgk(f, a, zeros(i1); atol=atol, rtol=rtol, order=order)
 
-	i1max = i1 + pren
-	while i1 < i1max
-		I, E = quadgk(f, zeros(i1), zeros(i1+1); atol=atol,
-			      rtol=rtol, order=order)
-		presum += I
-		Err += E
-		i1 += 1
-	end
+    i1max = i1 + pren
+    while i1 < i1max
+        I, E = quadgk(f, zeros(i1), zeros(i1+1); atol=atol,
+                      rtol=rtol, order=order)
+        presum += I
+        Err += E
+        i1 += 1
+    end
 
-	z0 = zeros(i1)
-	i = 1
-	ansI = T(0)
-	ansE = T(0)
-	oldansI = T(0)
-	ak = T[]
-	ek = T[]
-	while nconvergences > 0
-		z1 = zeros(i+i1)
-		I, E = quadgk(f, z0, z1; atol=atol, rtol=rtol)
-		push!(ak, I)
-		push!(ek, E)
-		ansI = accel_cohen_villegas_zagier(ak)
-		ansE = accel_cohen_villegas_zagier(ek)
+    z0 = zeros(i1)
+    i = 1
+    ansI = T(0)
+    ansE = T(0)
+    oldansI = T(0)
+    ak = T[]
+    ek = T[]
+    while nconvergences > 0
+        z1 = zeros(i+i1)
+        I, E = quadgk(f, z0, z1; atol=atol, rtol=rtol)
+        push!(ak, I)
+        push!(ek, E)
+        ansI = accel_cohen_villegas_zagier(ak)
+        ansE = accel_cohen_villegas_zagier(ek)
 
-		adiff = abs(ansI - oldansI)
-		rdiff = adiff * 2 / abs(ansI + oldansI)
-		if adiff <= atol || rdiff <= rtol
-			nconvergences -= 1
-			#@show i,adiff,rdiff,ansI+presum
-		end
-		oldansI = ansI
+        adiff = abs(ansI - oldansI)
+        rdiff = adiff * 2 / abs(ansI + oldansI)
+        if adiff <= atol || rdiff <= rtol
+            nconvergences -= 1
+            #@show i,adiff,rdiff,ansI+presum
+        end
+        oldansI = ansI
 
-		z0 = z1
-		i += 1
-	end
-	I = ansI + presum
-	E = Err + ansE
-	return I, E
+        z0 = z1
+        i += 1
+    end
+    I = ansI + presum
+    E = Err + ansE
+    return I, E
 end
 
 
 end # module
+
+
+# vim: set sw=4 et sts=4 :
